af7f9d76188352ef13a5d53e8a58159dbfb0691c
UKnow-IKnow <sarojsahu014@gmail.com>
2023-06-08 17:06:21 +0530

    tooltip working fine on button

--- app/src/main/java/com/example/inapptooltip/FirstFragment.kt
+++ app/src/main/java/com/example/inapptooltip/FirstFragment.kt
@@ -5,18 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.findNavController
+import com.example.inapptooltip.databinding.FragmentFirstBinding
 
 
 class FirstFragment : Fragment() {
 
-
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        setUpAdapter()
+
+        return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val binding = FragmentFirstBinding.bind(view)
+        _binding = binding
+
+        binding.RenderBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+
+    private fun setUpAdapter() {
+        val buttons = resources.getStringArray(R.array.buttons)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, buttons)
+        binding?.autoCompleteTextView?.setAdapter(arrayAdapter)
+    }
 }
--- app/src/main/java/com/example/inapptooltip/SecondFragment.kt
+++ app/src/main/java/com/example/inapptooltip/SecondFragment.kt
@@ -1,19 +1,51 @@
 package com.example.inapptooltip
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.inapptooltip.databinding.FragmentFirstBinding
+import com.example.inapptooltip.databinding.FragmentSecondBinding
+import com.example.tool_tip.Tooltip
+import com.example.tool_tip.utils.Position
 
 
 class SecondFragment : Fragment() {
 
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentSecondBinding.bind(view)
+        _binding = binding
+
+        binding.button3.setOnClickListener {
+            Tooltip.on(binding.button3)
+                .text("This is what you want")
+
+                .minHeight(30)
+                .minWidth(70)
+                .iconStart(android.R.drawable.ic_dialog_info)
+                .iconStartSize(30, 30)
+                .color(resources.getColor(R.color.purple_200))
+                .border(Color.BLACK, 1f)
+                .clickToHide(true)
+                .corner(15)
+                .position(Position.END)
+                .arrowSize(30,30)
+                .show(3000)
+        }
     }
 }
