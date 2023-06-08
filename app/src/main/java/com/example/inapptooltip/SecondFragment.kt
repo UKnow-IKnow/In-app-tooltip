d246616656fadd8d791c8d89b68d30548012689c
UKnow-IKnow <sarojsahu014@gmail.com>
2023-06-09 02:08:05 +0530

    Done with the assignment

--- app/src/main/java/com/example/inapptooltip/SecondFragment.kt
+++ app/src/main/java/com/example/inapptooltip/SecondFragment.kt
@@ -16,14 +16,13 @@ class SecondFragment : Fragment() {
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding
 
-    private val args : SecondFragmentArgs by  navArgs()
+    private val args: SecondFragmentArgs by navArgs()
 
     private var tooltip: Tooltip? = null
     private var showToolTip = false
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
@@ -49,25 +48,41 @@ class SecondFragment : Fragment() {
 
 
         //render tooltip
-        if (!showToolTip){
+        if (!showToolTip) {
             binding.button3.postDelayed(
                 {
-                    Tooltip.on(binding.button3)
-                        .text(ttText)
-                        .textSize(textSize)
-                        .minHeight(30)
-                        .minWidth(ttWidth)
-                        .iconStart(android.R.drawable.ic_dialog_info)
-                        .iconStartSize(30, 30)
-                        .color(resources.getColor(R.color.black))
-                        .clickToHide(true)
-                        .corner(radius)
-                        .position(Position.END)
-                        .arrowSize(arrHeight,arrWidth)
-                        .show(3000)
+                    Tooltip.on(binding.button3).text(ttText).textSize(textSize).minHeight(30)
+                        .minWidth(ttWidth).iconStart(android.R.drawable.ic_dialog_info)
+                        .iconStartSize(30, 30).color(resources.getColor(R.color.black))
+                        .clickToHide(true).corner(radius).position(Position.TOP)
+                        .arrowSize(arrHeight, arrWidth).show(3000)
                     showToolTip = true
-                },1000
+                }, 1000
             )
+        }
+
+        //button1
+        binding.button1.setOnClickListener {
+            Tooltip.on(binding.button1).text("I am excited to work with Plotline.").textSize(10f)
+                .minHeight(30).minWidth(100).iconEnd(R.drawable.happy_face).iconStartSize(30, 30)
+                .color(resources.getColor(R.color.black)).clickToHide(true).corner(20)
+                .position(Position.END).arrowSize(arrHeight, arrWidth).show(3000)
+        }
+
+        //button2
+        binding.button2.setOnClickListener {
+            Tooltip.on(binding.button2).text("I look forward to the next steps in the process.")
+                .textSize(10f).minHeight(30).minWidth(100).iconEnd(R.drawable.happy_face)
+                .iconStartSize(30, 30).color(resources.getColor(R.color.black)).clickToHide(true)
+                .corner(20).position(Position.START).arrowSize(arrHeight, arrWidth).show(3000)
+        }
+
+        //button4
+        binding.button4.setOnClickListener {
+            Tooltip.on(binding.button4).text("Give me a chance, and I will prove my skills.")
+                .textSize(10f).minHeight(30).minWidth(100).iconEnd(R.drawable.happy_face)
+                .iconStartSize(30, 30).color(resources.getColor(R.color.black)).clickToHide(true)
+                .corner(20).position(Position.TOP).arrowSize(arrHeight, arrWidth).show(3000)
         }
     }
 }
